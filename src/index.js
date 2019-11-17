f6937b58443cb8c0f7a98fc764a6b497403d0b19
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-17 18:38:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -26,7 +26,10 @@ Order.propTypes = {
   cone: PropTypes.bool,
   size: PropTypes.string,
   scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
-  orderInfo: PropTypes.object.isRequired
+  orderInfo: PropTypes.shape({
+    customerName: PropTypes.string.isRequired,
+    orderedAt: PropTypes.number.isRequired
+  }).isRequired
 };
 
 
